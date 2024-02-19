617ad4942c804be06fbdda47660f4f572c73e371
Diego-E2 <125125483+Diego-E2@users.noreply.github.com>
2024-02-18 19:19:16 -0600

    Prueba2
    Tiene que funcionar

---
+++ src/Listas_Simples/Lista_Simple.java
@@ -0,0 +1,13 @@
+package Listas_Simples;
+
+public class Lista_Simple {
+
+	 Nodo cabeza;//referencia al primer nodo de la lista
+	 
+	 //constructor para inicializar la lista como vacia 
+	 public Lista_Simple() {
+		 this.cabeza=null; //inicializamos la lista vacia
+	 }
+	
+	 
+}
---
+++ src/Listas_Simples/Nodo.java
@@ -0,0 +1,14 @@
+package Listas_Simples;
+
+public class Nodo {
+	int dato;
+	Nodo siguiente;  //referencia al siguiente nodo en la lista
+	
+	//Constructor
+	public Nodo(int dato) {
+		this.dato=dato;//inicializamos el nodo
+		this.siguiente=null;//inicializamos el siguiente nodo como nulo
+		
+	}
+
+}
